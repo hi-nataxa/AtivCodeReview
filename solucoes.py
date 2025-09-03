22c3b9041d6756f8519d373de6b124213ccb6bf0
hi-nataxa <nathasha.0407@gmail.com>
2025-09-03 18:59:26 -0300

    setup inicial do projeto

---
+++ solucoes.py
@@ -0,0 +1,10 @@
+def sao_anagramas(string1, string2):
+    # TODO: implementar logica
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # TODO: implementar logica
+    pass
+def valida_cpf(cpf_string):
+    # TODO: implementar logica
+    pass
